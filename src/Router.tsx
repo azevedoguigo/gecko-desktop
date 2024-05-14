8306570520091ddf70fca515f6d6c4d6406a3b37
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2024-05-14 13:02:09 -0300

    refactor: home page is now root directory.

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 
 import LoginPage from './pages/LoginPage'
+import HomePage from "./pages/HomePage"
 
 function Router() {
   return(
     <Routes>
-      <Route path="/" element={ <LoginPage /> } />
+      <Route path="/" element={ <HomePage /> } />
+      <Route path="/login" element={ <LoginPage /> } />
     </Routes>
   )
 }
