a1ed5e075603041745a6a9da1bbfd51cf6a25152
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2024-06-12 22:15:35 -0300

    :recycle: refactor: redirect user to login page if don't have token in local storage.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
+
 function HomePage() {
+  const navigate = useNavigate()
+
+  const token = localStorage.getItem("token")
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/login")
+    }
+  })
   return(
     <div>
       Home page
