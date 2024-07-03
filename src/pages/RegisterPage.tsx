22a8e2b1bade2bbea8eec7c6b1336a7ebbcc4323
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2024-07-03 18:16:09 -0300

    refactor: using icon components.

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -4,6 +4,9 @@ import UserApi from "../api/User.api"
 import api from "../config/api"
 import { ICreateUserRequestError } from "../types/api/UserApi.types"
 import { toast } from "react-toastify"
+import UserIcon from "../components/Icons/UserIcon"
+import EmailIcon from "../components/Icons/EmailIcon"
+import KeyIcon from "../components/Icons/KeyIcon"
 
 const userApi = new UserApi(api)
 
@@ -46,7 +49,7 @@ function RegisterPage() {
         <h1 className="flex justify-center text-2xl mb-4 font-semibold">Create Account</h1>
 
         <label className="input input-bordered flex items-center gap-2 mb-4">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
+          <UserIcon />
           <input 
             type="text" 
             className="grow" 
@@ -56,7 +59,7 @@ function RegisterPage() {
         </label>
 
         <label className="input input-bordered flex items-center gap-2 mb-4">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+          <EmailIcon />
           <input 
             type="text" 
             className="grow" 
@@ -66,7 +69,7 @@ function RegisterPage() {
         </label>
 
         <label className="input input-bordered flex items-center gap-2 mb-4">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
+          <KeyIcon />
           <input 
             type="password" 
             className="grow" 
